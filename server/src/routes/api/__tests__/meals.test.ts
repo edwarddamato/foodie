818caf6b10e77b301dea6031b95cfc4ec65f5225
Edward D'Amato <info@edwarddamato.com>
2017-07-02 17:19:10 +0100

    Add test for api/meals route action
    This presented a challenge because action takes
    an object of type Response as one of its parameters.
    Thus, TypeMoq was introduced as a library to facilitate
    mocking of types.

--- server/src/routes/api/__tests__/meals.test.ts
+++ server/src/routes/api/__tests__/meals.test.ts
@@ -1,10 +1,14 @@
+import * as TypeMoq from 'typemoq';
 import { Request, Response } from 'express';
+import { meals } from '../../../../data/meals'
 import apiMeals from '../meals';
 
 describe('apiMeals route action', () => {
-  it.skip('should send a list of meals in the response', () => {
-    apiMeals.action(null, null)
-
-    expect(1).toBe(1);
+  it('should send a list of meals in the response', () => {
+    const mockRequest: TypeMoq.IMock<Response> = TypeMoq.Mock.ofType<Response>();
+    mockRequest.send = jest.fn();
+    return apiMeals.action(null, mockRequest).then(() => {
+      expect(mockRequest.send).toHaveBeenCalledWith({ meals });
+    });
   });
 });
